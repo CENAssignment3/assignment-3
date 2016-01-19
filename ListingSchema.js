2cfeeabe55cf37590dca3f2ae6116f82c2bd1100
James Risberg <jimmyrisberg56@gmail.com>
2016-01-19 13:19:16 -0500

    Add schema for directory listings

--- ListingSchema.js
+++ ListingSchema.js
@@ -4,7 +4,15 @@ var mongoose = require('mongoose'),
 
 /* Create your schema */
 var listingSchema = new Schema({
-  /* your code here */
+    code: String, 
+    name: String, 
+    coordinates: {
+        latitude: Number, 
+        longitude: Number
+    }, 
+    address: String,
+    created_at: Date,
+    updated_at: Date
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
